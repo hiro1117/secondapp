b20175c4dbe2cf6207c21512d839de02d565463f
橋本紘樹 <k1n0hirokun797111genki@docomo.ne.jp>
2016-04-20 22:43:46 +0900

    機能の追加：segue

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var handle: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        
+        resultViewController.name = handle.text!
+    
+    
+        
+    }
 
-
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+    
 }
 
